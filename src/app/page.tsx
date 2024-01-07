48ca8f3d3beb22ab64a92665b0bbbcb5aa23f60d
carlos <carlosramos10k@gmail.com>
2024-01-07 18:59:12 -0300

    erros nos texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
   return (
     <main className={`w-full`}>
       <Container>
-        <h1 className={`text-center font-bold text-xl mt-8 mb-5`}>Separamos um jogo exclusico para você</h1>
+        <h1 className={`text-center font-bold text-xl mt-8 mb-5`}>Separamos um jogo exclusivo para você</h1>
         <Link href={`/game/${dailyGame.id}`}>
           <section className={`w-full bg-black rounded-lg`}>
           <div className={`w-full max-h-96 h-96 relative rounded-lg`}>
